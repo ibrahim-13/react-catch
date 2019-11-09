83dc0abe894fa781cafc1e760374009032458a93
ibrahim-13 <ib.arshad777@gmail.com>
2019-11-09 12:58:43 +0600

    Updated Example

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import App from './App';
+import ReactCatcher from 'react-catcher';
 
-import './index.css'
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render((
+  <ReactCatcher
+    fallback={
+      <h1>
+        An Error has occured !!
+      </h1>
+    }
+  >
+    <App />
+  </ReactCatcher>
+), document.getElementById('root'))
